1c0d379a020ebddc6321b05317c531e49e28d81b
Oscar <ohohurtado16@gmail.com>
2023-12-08 18:39:40 -0500

    feat :sparkles: mongoose installed

--- src/Index.js
+++ src/Index.js
@@ -4,4 +4,9 @@ const app =  express();
 
 const port =process.env.PORT || 9000;
 
+//routes
+
+app.get('/', (req,res) =>
+    res.send('Welcome to my api '))
+
 app.listen(port, () => console.log('Servidor funcionando en el puerto:', port ))
