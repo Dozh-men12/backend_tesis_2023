bf252b4808fc154e07c0084f766e800646d24e52
Oscar <ohohurtado16@gmail.com>
2023-12-08 20:23:27 -0500

    feat :sparkles: cors installed , change package npm to pnpm

--- src/Index.js
+++ src/Index.js
@@ -1,12 +1,21 @@
 const express = require ('express') ;
+const mongoose = require ('mongoose');
+
+
 
 const app =  express();
-
 const port =process.env.PORT || 9000;
 
 //routes
 
-app.get('/', (req,res) =>
-    res.send('Welcome to my api '))
+app.get('/', (req,res) => {
+    res.send("Welcome to my API");
+});
+    
+
+//Mongo DB connection 
+
+mongoose.connect()
+    
 
 app.listen(port, () => console.log('Servidor funcionando en el puerto:', port ))
